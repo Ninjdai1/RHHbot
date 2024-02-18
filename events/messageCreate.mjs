fa968c52de1914b0668751eeee4814231a6ae526
Ninjdai <ninjdai@duck.com>
2024-02-18 08:33:34 +0100

    Avoid trying to fetch PRs if bot cannot send messages in tag's channel

--- events/messageCreate.mjs
+++ events/messageCreate.mjs
@@ -1,10 +1,14 @@
+import { PermissionsBitField } from "discord.js";
 import { generatePRComponentsFromMessage } from "../utils/github.mjs";
 
 export default {
     name: "messageCreate",
     async execute(message, client) {
-        if (message.author.bot) return;
-        const matchComponents = await generatePRComponentsFromMessage(message, client);
+        if (message.author.bot || !message.guild.members.me.permissionsIn(message.channel).has(PermissionsBitField.Flags.SendMessages)) return;
+        const matchComponents = await generatePRComponentsFromMessage(
+            message,
+            client,
+        );
         if (matchComponents.length > 0) {
             const reply = await message.reply({
                 components: matchComponents.slice(0, 5),
